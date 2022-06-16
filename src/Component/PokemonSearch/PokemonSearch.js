2892ab796bddb77eebe529d5e8098706011bd1e5
Ahmed Rifat <rifatbafsc@gmail.com>
2022-06-16 10:01:42 +0600

    detals added

--- src/Component/PokemonSearch/PokemonSearch.js
+++ src/Component/PokemonSearch/PokemonSearch.js
@@ -25,8 +25,8 @@ const PokemonSearch = () => {
                     <div>
                         {/*search area start */}
 
-                  <input id='pokimon' type="text" placeholder="Find your pokemon" class="input input-bordered input-info w-full max-w-xs mt-40" />
-                  <button onClick={()=>pokimon()} class="btn btn-accent mt-3">Button</button>
+                  <input id='pokimon' type="text" placeholder="Find your pokemon" className="input input-bordered input-info w-full max-w-xs mt-40" />
+                  <button onClick={()=>pokimon()} className="btn btn-accent mt-3">Search</button>
                   
                   {/* search area start */}
                     </div>
@@ -35,18 +35,20 @@ const PokemonSearch = () => {
                   {/* pokimon card start */}
 
                     {(pokemon?.name)?
-                    <div class="card w-96 bg-base-100 shadow-xl mt-4 ">
-                    <figure class="px-10 pt-10">
-                        <img src={pokemon?.sprites?.other?.dream_world?.front_default} alt="pokemon" class="rounded-xl" />
+                    <div className="card w-96 bg-base-100 shadow-xl mt-5 mb-16 ">
+                    <figure className="px-10 pt-10">
+                        <img src={pokemon?.sprites?.other?.dream_world?.front_default} alt="pokemon" className="rounded-xl" />
                     </figure>
-                    <div class="card-body items-center text-center">
-                        <h2 class="card-title">{pokemon?.name}</h2>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">{pokemon?.name}</h2>
                         <p>Moves: {pokemon?.moves?.[0].move?.name}</p>
                         <p>Weight: {pokemon?.weight}</p>
+                        <p>Ability: {pokemon?.abilities?.[0].ability?.name}</p>
+                        <p>base-experience: {pokemon?.base_experience}</p>
                         
                     </div>
                     </div>:
-                    <div class="card lg:card-side bg-base-100 shadow-xl mt-5 mb-16">
+                    <div className="card lg:card-side bg-base-100 shadow-xl mt-5 mb-16">
                     <figure><img src="https://s.clipartkey.com/mpngs/s/54-542290_transparent-pokeball-png-transparent-background-pokeball-opening.png" alt="Album"/></figure>
                   </div>
                     
